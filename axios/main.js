84deae63a88da2353ac13fb87deedd6910913a06
Kelvin <et.kelvin@gmail.com>
2022-01-08 23:24:39 +0800

    增加示例页面，测试请求用例

--- axios/main.js
+++ axios/main.js
@@ -1,8 +1,28 @@
 import "./style.css";
 import axios from "./lib/axios";
 
-axios
-  .get("https://jsonplaceholder.typicode.com/todos/1")
-  .then((res) => console.log(res));
+const TEST_BASE_URL = "https://jsonplaceholder.typicode.com/";
+const GET_API = "posts/1";
 
-console.dir(axios);
+const getExampleButton = document.getElementById("get-example");
+
+getExampleButton.addEventListener("click", () => {
+  const url = TEST_BASE_URL + GET_API;
+  axios.get(url).then((res) => {
+    alert(res);
+  });
+});
+
+const interceptorExampleButton = document.getElementById("interceptor-example");
+
+interceptorExampleButton.addEventListener("click", () => {
+  const url = TEST_BASE_URL + GET_API;
+  const instance = axios.create();
+  instance.interceptors.request.use((config) => {
+    alert("这是拦截器");
+    return config;
+  }, null);
+  instance.get(url).then((res) => {
+    alert(res);
+  });
+});
