b9fc726f81fb4be5d0402832d10c413b105344eb
Kelvin <et.kelvin@gmail.com>
2022-01-09 23:25:16 +0800

    完善示例请求用例

--- axios/main.js
+++ axios/main.js
@@ -3,12 +3,29 @@ import axios from "./lib/axios";
 
 const TEST_BASE_URL = "https://jsonplaceholder.typicode.com/";
 const GET_API = "posts/1";
+const GET_TEST_URL = TEST_BASE_URL + GET_API;
 
 const getExampleButton = document.getElementById("get-example");
+const getInstanceExampleButton = document.getElementById(
+  "get-example-instance"
+);
+const getMethodExampleButton = document.getElementById("get-example-method");
 
 getExampleButton.addEventListener("click", () => {
-  const url = TEST_BASE_URL + GET_API;
-  axios.get(url).then((res) => {
+  axios(GET_TEST_URL).then((res) => {
+    alert(res);
+  });
+});
+
+getInstanceExampleButton.addEventListener("click", () => {
+  const instance = axios.create();
+  instance.get(GET_TEST_URL).then((res) => {
+    alert(res);
+  });
+});
+
+getMethodExampleButton.addEventListener("click", () => {
+  axios.get(GET_TEST_URL).then((res) => {
     alert(res);
   });
 });
@@ -16,13 +33,12 @@ getExampleButton.addEventListener("click", () => {
 const interceptorExampleButton = document.getElementById("interceptor-example");
 
 interceptorExampleButton.addEventListener("click", () => {
-  const url = TEST_BASE_URL + GET_API;
   const instance = axios.create();
   instance.interceptors.request.use((config) => {
     alert("这是拦截器");
     return config;
   }, null);
-  instance.get(url).then((res) => {
+  instance.get(GET_TEST_URL).then((res) => {
     alert(res);
   });
 });
