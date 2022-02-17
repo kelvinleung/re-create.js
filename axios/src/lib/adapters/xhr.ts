068acad43b3c8fd7ecb266cbf0cd555ad9d8b2f4
Kelvin <et.kelvin@gmail.com>
2022-02-17 23:27:29 +0800

    增加取消请求的注释

--- axios/src/lib/adapters/xhr.ts
+++ axios/src/lib/adapters/xhr.ts
@@ -1,4 +1,3 @@
-import Cancel from "../cancel/Cancel";
 import { AxiosRequestConfig, ResponsePromise } from "../types";
 
 export default function xhrAdapter(
@@ -9,37 +8,36 @@ export default function xhrAdapter(
     let xhr: XMLHttpRequest | null = new XMLHttpRequest();
     xhr.open(method!.toUpperCase(), url!);
     xhr.onreadystatechange = function handleLoad() {
-      if (!xhr) {
+      if (!xhr || xhr.readyState !== 4) {
         return;
       }
-      if (xhr.readyState !== 4) {
-        return;
-      }
+      const response = {
+        data: xhr.response,
+        status: xhr.status,
+        statusText: xhr.statusText,
+        config,
+      };
       if (xhr.status >= 200 && xhr.status < 300) {
-        const response = {
-          data: xhr.response,
-          status: xhr.status,
-          statusText: xhr.statusText,
-          config,
-        };
         resolve(response);
       } else {
-        reject(new Error(`${xhr.status}`));
+        reject(new Error("出错了"));
       }
       xhr = null;
     };
     xhr.onabort = function handleAbort() {
+      // 这里是非“手动”触发的取消，浏览器行为
       if (!xhr) {
         return;
       }
-      console.log("abort");
       reject(new Error("Request aborted"));
       xhr = null;
     };
     if (cancelToken) {
-      cancelToken.promise.then((msg) => {
-        reject(new Cancel(msg.message));
+      cancelToken.promise.then((cancel) => {
+        // 要先 reject 再 abort，先 abort 会导致 readystate 变为 4，status 为 0，走 onreadystatechange 的逻辑
+        reject(cancel);
         xhr!.abort();
+        xhr = null;
       });
     }
     xhr.send();
