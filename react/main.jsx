464e74c69e5de813b50f98963b8439631629045e
Kelvin <et.kelvin@gmail.com>
2022-05-29 23:51:47 +0800

    页面用例增加事件操作

--- react/main.jsx
+++ react/main.jsx
@@ -1,13 +1,21 @@
 // “假” React，esbuild 默认为解析 jsx 文件
 import React from "./react";
 
-const element = (
-  <div style="background: salmon">
-    <h1>Hello World</h1>
-    <h2 style="text-align:right">from fake React</h2>
-  </div>
-);
-
 const container = document.getElementById("app");
 
-React.render(element, container);
+const updateValue = (e) => {
+  rerender(e.target.value);
+};
+
+const rerender = (value) => {
+  console.log(value);
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value} />
+      <h2>Hello {value}</h2>
+    </div>
+  );
+  React.render(element, container);
+};
+
+rerender("World");
