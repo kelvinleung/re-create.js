af86c13a8d2d99821ead8c2c33bbd504d7d17ef5
Kelvin <et.kelvin@gmail.com>
2022-05-30 18:24:42 +0800

    修改页面支持测试多个用例

--- react/main.jsx
+++ react/main.jsx
@@ -1,21 +1,37 @@
 // “假” React，esbuild 默认为解析 jsx 文件
 import React from "./react";
 
-const container = document.getElementById("app");
+function renderReconsiliation() {
+  const container = document.getElementById("reconciliation");
 
-const updateValue = (e) => {
-  rerender(e.target.value);
-};
+  const updateValue = (e) => {
+    rerender(e.target.value);
+  };
 
-const rerender = (value) => {
-  console.log(value);
-  const element = (
-    <div>
-      <input onInput={updateValue} value={value} />
-      <h2>Hello {value}</h2>
-    </div>
-  );
+  const rerender = (value) => {
+    console.log(value);
+    const element = (
+      <div>
+        <input onInput={updateValue} value={value} />
+        <h2>Hello {value}</h2>
+      </div>
+    );
+    React.render(element, container);
+  };
+
+  rerender("World");
+}
+
+function renderFunctionComponent() {
+  function App(props) {
+    return <h1>Hi {props.name}</h1>;
+  }
+  const element = <App name="foo" />;
+
+  const container = document.getElementById("function-component-root");
+
   React.render(element, container);
-};
+}
 
-rerender("World");
+// renderReconsiliation();
+renderFunctionComponent();
